642e4eb54c4a7b2867061bc3b6ccaaff17e0927d
Michael Yang <myang@viantinc.com>
2015-10-19 09:57:05 -0700

    Added the code which is equivalent to SparkJava on createAPost except GetAllPosts

--- src/main/java/com/myang/App.java
+++ src/main/java/com/myang/App.java
@@ -2,24 +2,16 @@ package com.myang;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+
 
 /**
  * Hello world!
  *
  */
 
-@Controller
-@EnableAutoConfiguration
+@SpringBootApplication
 public class App 
 {
-  @RequestMapping("/")
-  @ResponseBody
-  String home() {
-      return "Hello World!";
-  }
-
   public static void main(String[] args) throws Exception {
       SpringApplication.run(App.class, args);
   }
---
+++ src/main/java/com/myang/controller/PostController.java
@@ -0,0 +1,92 @@
+package com.myang.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.myang.dao.Impl.PostDAOImpl;
+import com.myang.dao.PostDAO;
+import com.myang.dto.NewPostPayloadDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * Created by myang on 10/16/15.
+ */
+
+@RestController
+public class PostController
+{
+  //Data persists in Memory.  There is no DB involved for now
+  private PostDAO postDao = new PostDAOImpl();
+  public Gson gson = new Gson();
+
+
+  @RequestMapping(method= RequestMethod.GET, value="/helloworld")
+  @ResponseBody
+  public String helloWorld()
+  {
+    return "Hello World!";
+  }
+
+  /*
+  localhost:8080/hello
+  localhost:8080/hello?name=You
+  */
+  @RequestMapping(method = RequestMethod.GET, value = "/hello")
+  @ResponseBody
+  public String sayHelloToSomeone(@RequestParam(value="name", defaultValue="World") String name)
+  {
+    return "Hello " + name + "!";
+  }
+
+  //Use ResponseEntity to write the HTTP Status
+  //-Reference-
+  //http://stackoverflow.com/questions/26134331/how-to-respond-with-http-status-code-in-a-spring-mvc-restcontroller-responsebo
+  //-Reference about writing a response in Spring-
+  //http://stackoverflow.com/questions/3146461/does-spring-mvc-have-response-write-to-output-to-the-browser-directly
+  @RequestMapping(method = RequestMethod.POST, value = "/createAPost")
+  public ResponseEntity<?> createAPost(HttpServletResponse response, @RequestBody(required = true) String newPostObject)
+  {
+    try {
+      ObjectMapper mapper = new ObjectMapper();
+      NewPostPayloadDTO newPostData = mapper.readValue(newPostObject, NewPostPayloadDTO.class);
+      if (!newPostData.isValid()) {
+
+        //response.status(HTTP_BAD_REQUEST);
+        //return "";
+        return new ResponseEntity<String>("Invalid Input Object", HttpStatus.BAD_REQUEST);
+      }
+      int id = postDao.createPost(newPostData.getTitle(), newPostData.getContent(), newPostData.getCategories());
+
+      //response.status(200);
+      //response.type("application/json");
+      return new ResponseEntity<String>(""+id, HttpStatus.OK);
+    } catch (IOException ex) {
+      //response.status(HTTP_BAD_REQUEST);
+      return new ResponseEntity<String>("Request Body does not match with DTO Object", HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  //Spring dictates the HTTP status code
+  @RequestMapping(method = RequestMethod.POST, value = "/createPost")
+  public String createAPost(@RequestBody(required = true) String newPostObject)
+  {
+    try {
+      ObjectMapper mapper = new ObjectMapper();
+      NewPostPayloadDTO newPostData = mapper.readValue(newPostObject, NewPostPayloadDTO.class);
+
+      int id = postDao.createPost(newPostData.getTitle(), newPostData.getContent(), newPostData.getCategories());
+
+      //response.status(200);
+      //response.type("application/json");
+      return ""+id;
+    } catch (IOException ex) {
+      //response.status(HTTP_BAD_REQUEST);
+      return "Request Body does not match with DTO Object but HTTP status is still 200 instead of 400 in PostMan";
+    }
+  }
+}
